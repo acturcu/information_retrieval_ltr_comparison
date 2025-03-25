94703d2d1d007b3808efcc600a3364aa000b9ac0
SebastianManda <sebimanda@yahoo.com>
2025-03-25 15:18:15 +0100

    Fix lambda rank

--- models/lambdaRank.py
+++ models/lambdaRank.py
@@ -3,10 +3,13 @@ from lightgbm import LGBMRanker
 
 def get_lambdaRank_model(base_model: pt.terrier.FeaturesRetriever):
     return base_model >> pt.ltr.apply_learned_model(
-    LGBMRanker(
-        metric="ndcg",
-        importance_type="gain",
-        rank="pairwise",
-    ),
-    form="ltr",
-)
+        LGBMRanker(
+            objective="lambdarank",
+            metric="ndcg",
+            learning_rate=0.05,
+            num_leaves=16,
+            boosting_type="gbdt",
+            index="features",
+        ),
+        form="ltr",
+    )
